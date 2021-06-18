e901665ba00871db601a000c3bb3dbe2f1a4617e
Deepshikha Mishra <>
2021-06-18 22:44:08 +0530

    init

---
+++ config.js
@@ -0,0 +1,6 @@
+
+module.exports ={
+    MONGODB : 'mongodb+srv://deepshikha:deepshikha@cluster0.p6jlt.mongodb.net/merng?retryWrites=true',
+    SECRET_KEY: 'some very secret key'
+}
+// &w=majority myFirstDatabase
